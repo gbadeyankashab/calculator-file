85cd1504b4f7a1f2cc9f91f635ebf56ec41f4c21
dimeji <gbadeyankashaban24@gmail.com>
2022-08-07 21:03:39 +0100

    calculator changes

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ let displays;
 let display,display1,display2,answer;
 
 function displayNumbers(number) {
-    document.getElementById('mainDisplay').innerHTML+=number;
-    display=Number(document.getElementById('mainDisplay').innerHTML);
+    document.getElementById("mainDisplay").innerHTML+=number;
+    display=Number(document.getElementById("mainDisplay").innerHTML);
     if (sign) {
         display2=display;
     }
@@ -17,69 +17,65 @@ function displayNumbers(number) {
     }
     
 }
-function operator(operatorr) {
+function operator(operators) {
 if (display1 && sign ) {
     equal()
-    document.getElementById('secondaryDisplay').innerHTML='';
-    document.getElementById('mainDisplay').innerHTML=(answer);
+    document.getElementById("secondaryDisplay").innerHTML='';
+    document.getElementById("mainDisplay").innerHTML=(answer);
     console.log(answer);
 }
-    sign=operatorr
-    input1=Number(document.getElementById('mainDisplay').innerHTML);
-    document.getElementById('secondaryDisplay').innerHTML+=(input1+sign);
-
-    input2=Number(document.getElementById('secondaryDisplay').innerHTML);
-    if (operatorr){
-        document.getElementById('mainDisplay').innerHTML='';
+    sign=operators
+    input1=Number(document.getElementById("mainDisplay").innerHTML);
+    document.getElementById("secondaryDisplay").innerHTML+=(input1+sign);
+    if (operators){
+        document.getElementById("mainDisplay").innerHTML='';
     }
 }
 
 
 
 function calculateResults(calculate) {
-    input=Number(document.getElementById('mainDisplay').innerHTML);
-    document.getElementById('secondaryDisplay').innerHTML+=(input+calculate);
+    input=Number(document.getElementById("mainDisplay").innerHTML);
+    document.getElementById("secondaryDisplay").innerHTML+=(input+calculate);
 
     if (sign=='+'){
         answer=display1+display2
-       Number( document.getElementById('mainDisplay').innerHTML=answer);
+       Number( document.getElementById("mainDisplay").innerHTML=answer);
   
     }
     else if (sign=='-'){
         answer=display1-display2
-        document.getElementById('mainDisplay').innerHTML=(answer);
+        document.getElementById("mainDisplay").innerHTML=(answer);
     }
    else if (sign=='*'){
         answer=display1*display2
-        document.getElementById('mainDisplay').innerHTML=(answer);
+        document.getElementById("mainDisplay").innerHTML=(answer);
     }
     else if (sign=='/'){
         answer=display1/display2
-        document.getElementById('mainDisplay').innerHTML=(answer);
+        document.getElementById("mainDisplay").innerHTML=(answer);
     }
 
 }
 
 //the C button
 function clearAll() {
-    val1=''
-    val2=''
-    answer=''
-    sign=''
-   document.getElementById('mainDisplay').innerHTML='';
-   document.getElementById('secondaryDisplay').innerHTML='';
-   
-
+    display1='';
+    display2='';
+    answer='';
+    sign='';
+   document.getElementById("mainDisplay").innerHTML='';
+   document.getElementById("secondaryDisplay").innerHTML='';
 }
 
 // the CE button
 function clearDisplay() {
-   document.getElementById('mainDispaly').innerHTML='';
+   document.getElementById("mainDispaly").innerHTML='';
 }
 
 //fa fa-delete button
 function deleteButton() {
-    displays=document.getElementById('mainDisplay').innerHTML;
+    displays=document.getElementById("mainDisplay").innerHTML;
     edit=displays.slice(0,-1)
-    document.getElementById('mainDisplay').innerHTML=edit;
+    document.getElementById("mainDisplay").innerHTML=edit;
 }
